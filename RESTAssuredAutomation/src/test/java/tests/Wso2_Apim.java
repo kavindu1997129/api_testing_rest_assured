26193ed7c591f4908650b46ef83d76752d4e734b
kavindu <kavindu1997129@gmail.com>
2022-09-07 10:53:53 +0530

    code for api version  changimg is added

--- RESTAssuredAutomation/src/test/java/tests/Wso2_Apim.java
+++ RESTAssuredAutomation/src/test/java/tests/Wso2_Apim.java
@@ -6,21 +6,23 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 
-import org.testng.TestNGUtils;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class Wso2_Apim {
 	
-	Response  res1, res2, res3, res4, res5, res6, res7;
+	Response  res1, res2, res3, res4, res5, res6, res7, res8;
 
 	FileInputStream input;
 	Properties p;
 	byte[] authplj;
 	byte[] apicreationplj;
+	byte[] createapiproductplj;
 	String authpls;
 	String apicreationpls;
+	String createapiproductpls;
+
 
 	String accessToken;
 
@@ -30,13 +32,17 @@ public class Wso2_Apim {
 
 		try {
 			String path =  "./src/test/resources/config.properties";
-			authplj = Files.readAllBytes(Paths.get("./src/test/payloads/payload.json"));
-			authpls = new String(authplj);
-			apicreationplj = Files.readAllBytes(Paths.get("./src/test/payloads/apicretioon_payload.json"));
-			apicreationpls = new String(apicreationplj);
 			p = new Properties();
 			input = new FileInputStream(path);
 			p.load(input);
+			
+			authplj = Files.readAllBytes(Paths.get("./src/test/payloads/payload.json"));
+			authpls = new String(authplj);
+			apicreationplj = Files.readAllBytes(Paths.get("./src/test/payloads/apicretion_payload.json"));
+			apicreationpls = new String(apicreationplj);
+			createapiproductplj = Files.readAllBytes(Paths.get("./src/test/payloads/creat_api_pprodt.json"));
+			createapiproductpls = new String(createapiproductplj);
+			
 
 		} catch (Exception e) {
 			System.out.println(e);
@@ -103,31 +109,29 @@ public class Wso2_Apim {
 				.auth()
 				.oauth2(accessToken)
 				.get(p.getProperty("publisher_url")+"/"+res4.jsonPath().get("list[0]['id']"));
-		
-		//System.out.println(res5.jsonPath().prettyPrint());
+	
+	}
+
+	@Test
+	public void thumbnail_adding() {
 
 		res7 = RestAssured.given()
 				.relaxedHTTPSValidation()
 				.auth()
 				.oauth2(accessToken)
 				.multiPart(new File("./src/test/payloads/thumbnail.jpg"))
-				//.multiPart("./src/test/payloads/thumbnail.jpg","file","application/json")
 				.put(p.getProperty("publisher_url")+"/"+res4.jsonPath().get("list[0]['id']")+"/thumbnail");
 
 		System.out.println(res7.jsonPath().prettyPrint());
-	}
-	// @Test 
-	// public void add_thumbnail_app(){
 
-	// 	res7 = RestAssured.given()
-	// 			.relaxedHTTPSValidation()
-	// 			.auth()
-	// 			.oauth2(accessToken)
-	// 			.multiPart("./src/test/payloads/thumbnail.jpg","application/json")
-	// 			.get(p.getProperty("publisher_url")+"/"+res4.jsonPath().get("list[0]['id']/thumbnail"));
+		res8= RestAssured.given()
+				.relaxedHTTPSValidation()
+				.auth()
+				.oauth2(accessToken)
+				.contentType("application/json")
+				.post(p.getProperty("publisher_url")+"/copy-api?newVersion=2.0.0&defaultVersion=false&apiId="+res4.jsonPath().get("list[0]['id']"));
 
-	// 	System.out.println(res6.jsonPath().prettyPrint());
-
-	// }
+		System.out.println(res8.jsonPath().prettyPrint());
+	}	
 
 }
