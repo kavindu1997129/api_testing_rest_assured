f68bd6e15bea9da4886e89564b46b5a1bcb79dad
WSO2 <kavindu1997129@gmail.com>
2022-10-25 00:51:49 +0530

    Working code (demo)

--- RESTAssuredAutomation/src/test/java/tests/DemoDataPopulation.java
+++ RESTAssuredAutomation/src/test/java/tests/DemoDataPopulation.java
@@ -83,107 +83,94 @@ public class DemoDataPopulation extends BaseTest{
       logger.info("[USER STORE]: User store related tests were completed");
       
   }
-    
-
   
-//  @Test
-//  @Parameters({"tenantAdminUser1","tenantAdminUser1Password", "apiCreationPayload",
-//      "createApiOpenApiDefinition","thumbnailImage","apiLifecycleStatusAction",
-//      "schemaGraphQlPayload","apiCretionPayloadGraphQL"})  
-//  public void test3_PublisherPortal(
-//          String tenantAdminUser, String tenantAdminUserPassword, String apiCreationPayload,
-//          String createApiOpenApiDefinition, String thumbnailImage, String apiLifecycleStatusAction,
-//          String schemaGraphQlPayload, String apiCretionPayloadGraphQL
-//          ) throws InterruptedException {
-//      
-//      authenticationObject.setUsername(tenantAdminUser);
-//      authenticationObject.setUserpassword(tenantAdminUserPassword);
-//      
-//      Authentication authentication = new Authentication(authenticationObject);
-//      String accessToken1 = authentication.getAccessToken();
-//      
-//      Publisher.Apis api = new Publisher.Apis(accessToken1, ApimVersions.APIM_3_2);
-//      
-////      Response createApiRes = api.createApi(ContentTypes.APPLICATION_JSON, apiCreationPayload);
-////      logger.info("Status Code [CREATE API]: "+createApiRes.statusCode());
-//       
-//      Response createApiOpenApiDefinitionRes = api.imporOpenAPIDefinition(createApiOpenApiDefinition, apiCreationPayload);
-//      logger.info("Status Code [CREATE OPEN API DEFINITION]: "+createApiOpenApiDefinitionRes.statusCode());
-//      String apiId = createApiOpenApiDefinitionRes.jsonPath().get("id");
-//      if(apiId != null && createApiOpenApiDefinitionRes.statusCode()==201) JsonReadWrite.addApiToJson(apiId);
-//      
-//      Response createGraphQlApiRes = api.importAPIDefinition(schemaGraphQlPayload, apiCretionPayloadGraphQL);
-//      logger.info("Status Code [CREATE GRAPHQL API]: "+createGraphQlApiRes.statusCode());
-//      String apiIdGraphQL = createGraphQlApiRes.jsonPath().get("id");
-//      if(apiIdGraphQL != null && createGraphQlApiRes.statusCode()==201) JsonReadWrite.addApiToJson(apiIdGraphQL);
-//      
-//      Response changeGraphQlApiStatusRes = api.changeApiStatus(JsonReadWrite.readApiId(1), apiLifecycleStatusAction);
-//      logger.info("Status Code [CHANGE GRAPHQL API STATUS]: "+changeGraphQlApiStatusRes.statusCode());
-//      
-//      Response searchApiRes = api.searchApis();
-//      logger.info("Status Code [SEARCH API]: "+searchApiRes.statusCode());
-//
-//      Response uploadApiThumbnailRes = api.uploadThumbnailImage(thumbnailImage, JsonReadWrite.readApiId(0));
-//      logger.info("Status Code [UPLOAD API THUMBNAIL]: "+uploadApiThumbnailRes.statusCode());
-//
-//      Response changeApiStatusRes = api.changeApiStatus(JsonReadWrite.readApiId(0), apiLifecycleStatusAction);
-//      logger.info("Status Code [CHANGE API STATUS]: "+changeApiStatusRes.statusCode());
-//      
-//      logger.info("[PUBLISHER PORTAL]: Dev Portal tests were completed");
-//      
-//  }
-//  
-//  @Test
-//  @Parameters({"tenantUser","tenantUserPassword","apiSearchingKeyWord",
-//      "appPayloadListAsString", "genarateKeyPayloadListAsString"})  
-//  public void test4_DevPortal(
-//          String tenantUser, String tenantUserPassword,String apiSearchingKeyWord, 
-//          String appPayloadListAsString, String genarateKeyPayloadListAsString) {
-//     
-//      ArrayList<String> appPayloadList = new ArrayList<String>(Arrays.asList(appPayloadListAsString.split(",")));
-//      ArrayList<String> genarateKeyPayload = new ArrayList<String>(Arrays.asList(genarateKeyPayloadListAsString.split(",")));
-//      
-//      authenticationObject.setUsername(tenantUser);
-//      authenticationObject.setUserpassword(tenantUserPassword);
-//      
-//      Authentication authentication = new Authentication(authenticationObject);
-//      accessToken = authentication.getAccessToken();
-//      
-//      DevPortal.UnfiedSearch dSearch = new DevPortal.UnfiedSearch(accessToken, ApimVersions.APIM_3_2);
-//      Response searchApiByName = dSearch.getApiAndApiDocumentByContent(apiSearchingKeyWord);
-//      logger.info("Status Code [SEARCHED API BY NAME]: "+searchApiByName.statusCode());
-//      
-//      DevPortal.Applications applications = new DevPortal.Applications(accessToken, ApimVersions.APIM_3_2);
-//      DevPortal.ApplicationKeys appKeys = new DevPortal.ApplicationKeys(accessToken, ApimVersions.APIM_3_2);
-//      DevPortal.Subscriptions subscription = new DevPortal.Subscriptions(accessToken, ApimVersions.APIM_3_2);
-//      
-//      for(int i = 0 ; i <appPayloadList.size() ; i++) {
-//          
-//          Response createNewApplicationRes = applications.createNewApplications(appPayloadList.get(i));
-//          logger.info("Status Code [CREATE NEW APPLICATION "+(i+1)+"]: "+createNewApplicationRes.statusCode());
-//          String appId = createNewApplicationRes.jsonPath().get("applicationId"); 
-//          if(appId != null && createNewApplicationRes.statusCode()==201) JsonReadWrite.addAppToJson(appId);
-//          
-//          Response subscribeRes = subscription.addNewSubscription("subscribeToApp.json",JsonReadWrite.readApiId(0),JsonReadWrite.readAppId(i));
-//          logger.info("Status Code [SUBSCRIBE TO API "+(i+1)+"]: "+subscribeRes.statusCode());
-//          if(subscribeRes.statusCode()==201) JsonReadWrite.addSubscriptionData(JsonReadWrite.readAppId(i), subscribeRes.jsonPath().prettify());
-//          
-//          Response subscribeGraphQlRes = subscription.addNewSubscription("subscribeToApp.json",JsonReadWrite.readApiId(1),JsonReadWrite.readAppId(i));
-//          logger.info("Status Code [SUBSCRIBE TO GRAPHQL API "+(i+1)+"]: "+subscribeGraphQlRes.statusCode());
-//        
-//          Response genSandboxKeyRes = appKeys.generateApplicationKeys(JsonReadWrite.readAppId(i), genarateKeyPayload.get(i));
-//          logger.info("Status Code [GENERATE ACCESS TOKEN "+(i+1)+"]: "+genSandboxKeyRes.statusCode());
-//          if(genSandboxKeyRes.statusCode()==200) JsonReadWrite.addKeys(JsonReadWrite.readAppId(i), "sandbox", genSandboxKeyRes.jsonPath().prettify());
-//      }
-//      
-//      Response removeSubscriptionRes = subscription.removeSubscription(JsonReadWrite.getSubscriptionId(JsonReadWrite.readAppId(0),0));
-//      if(removeSubscriptionRes.statusCode() == 200) JsonReadWrite.removeSubscriptionData(JsonReadWrite.readAppId(0), JsonReadWrite.getSubscriptionId(JsonReadWrite.readAppId(0),0));
-//      logger.info("Status Code [REMOVE SUSCRIPTION APP 3 OF API "+(1)+"]: "+removeSubscriptionRes.statusCode());
-//      
-//      logger.info("[DEV PORTAL]: Dev Portal tests were completed");
-//      
-//      
-//  }
+  @Test
+  @Parameters({"tenantUserPublisher","tenantUserPasswordPublisher", "apiCreationPayload",
+      "createApiOpenApiDefinition","thumbnailImage","apiLifecycleStatusAction"})  
+  public void test3_PublisherPortal(
+          String tenantUserPublisher, String tenantUserPasswordPublisher, String apiCreationPayload,
+          String createApiOpenApiDefinition, String thumbnailImage, String apiLifecycleStatusAction
+          ) throws InterruptedException {
+      
+      authenticationObject.setUsername(tenantUserPublisher);
+      authenticationObject.setUserpassword(tenantUserPasswordPublisher);
+      
+      Authentication authentication = new Authentication(authenticationObject);
+      String accessToken1 = authentication.getAccessToken();
+      
+      Publisher.Apis api = new Publisher.Apis(accessToken1, ApimVersions.APIM_3_2);
+       
+      Response createApiOpenApiDefinitionRes = api.imporOpenAPIDefinition(createApiOpenApiDefinition, apiCreationPayload);
+      logger.info("Status Code [CREATE OPEN API DEFINITION]: "+createApiOpenApiDefinitionRes.statusCode());
+      String apiId = createApiOpenApiDefinitionRes.jsonPath().get("id");
+      if(apiId != null && createApiOpenApiDefinitionRes.statusCode()==201) JsonReadWrite.addApiToJson(apiId);
+
+      Response uploadApiThumbnailRes = api.uploadThumbnailImage(thumbnailImage, JsonReadWrite.readApiId(0));
+      logger.info("Status Code [UPLOAD API THUMBNAIL]: "+uploadApiThumbnailRes.statusCode());
+
+      Response changeApiStatusRes = api.changeApiStatus(JsonReadWrite.readApiId(0), apiLifecycleStatusAction);
+      logger.info("Status Code [CHANGE API STATUS]: "+changeApiStatusRes.statusCode());
+      
+      Response searchApiRes = api.searchApis();
+      logger.info("Status Code [SEARCH API]: "+searchApiRes.statusCode());
+      
+      logger.info("[PUBLISHER PORTAL]: Dev Portal tests were completed");
+      
+  }
+  
+  @Test
+  @Parameters({"tenantUserCreator","tenantUserPasswordCreator","apiSearchingKeyWord",
+      "appPayloadListAsString", "genarateKeyPayloadListAsString"})  
+  public void test4_DevPortal(
+          String tenantUserCreator, String tenantUserPasswordCreator,String apiSearchingKeyWord, 
+          String appPayloadListAsString, String genarateKeyPayloadListAsString) {
+     
+      ArrayList<String> appPayloadList = new ArrayList<String>(Arrays.asList(appPayloadListAsString.split(",")));
+      ArrayList<String> genarateKeyPayload = new ArrayList<String>(Arrays.asList(genarateKeyPayloadListAsString.split(",")));
+      
+      authenticationObject.setUsername(tenantUserCreator);
+      authenticationObject.setUserpassword(tenantUserPasswordCreator);
+      
+      Authentication authentication = new Authentication(authenticationObject);
+      accessToken = authentication.getAccessToken();
+      
+      DevPortal.UnfiedSearch dSearch = new DevPortal.UnfiedSearch(accessToken, ApimVersions.APIM_3_2);
+      Response searchApiByName = dSearch.getApiAndApiDocumentByContent(apiSearchingKeyWord);
+      logger.info("Status Code [SEARCHED API BY NAME]: "+searchApiByName.statusCode());
+      
+      DevPortal.Applications applications = new DevPortal.Applications(accessToken, ApimVersions.APIM_3_2);
+      DevPortal.ApplicationKeys appKeys = new DevPortal.ApplicationKeys(accessToken, ApimVersions.APIM_3_2);
+      DevPortal.Subscriptions subscription = new DevPortal.Subscriptions(accessToken, ApimVersions.APIM_3_2);
+      
+      for(int i = 0 ; i <appPayloadList.size() ; i++) {
+          
+          Response createNewApplicationRes = applications.createNewApplications(appPayloadList.get(i));
+          String appId = createNewApplicationRes.jsonPath().get("applicationId"); 
+          if(appId != null && createNewApplicationRes.statusCode()==201) {
+              logger.info("Status Code [CREATE NEW APPLICATION "+(i+1)+"]: "+createNewApplicationRes.statusCode());
+              JsonReadWrite.addAppToJson(appId); 
+              Response subscribeRes = subscription.addNewSubscription("subscribeToApp.json",JsonReadWrite.readApiId(0),JsonReadWrite.readAppId(i));
+              logger.info("Status Code [SUBSCRIBE TO API "+(i+1)+"]: "+subscribeRes.statusCode());
+              if(subscribeRes.statusCode()==201) JsonReadWrite.addSubscriptionData(JsonReadWrite.readAppId(i), subscribeRes.jsonPath().prettify());
+            
+              Response genSandboxKeyRes = appKeys.generateApplicationKeys(JsonReadWrite.readAppId(i), genarateKeyPayload.get(i));
+              logger.info("Status Code [GENERATE ACCESS TOKEN "+(i+1)+"]: "+genSandboxKeyRes.statusCode());
+              if(genSandboxKeyRes.statusCode()==200) JsonReadWrite.addKeys(JsonReadWrite.readAppId(i), "sandbox", genSandboxKeyRes.jsonPath().prettify());        
+          } 
+          else {
+              logger.info("Status Code [CREATE NEW APPLICATION "+(i+1)+"]: API creation failed");
+          }
+           
+      }
+      
+      Response removeSubscriptionRes = subscription.removeSubscription(JsonReadWrite.getSubscriptionId(JsonReadWrite.readAppId(0),0));
+      if(removeSubscriptionRes.statusCode() == 200) JsonReadWrite.removeSubscriptionData(JsonReadWrite.readAppId(0), JsonReadWrite.getSubscriptionId(JsonReadWrite.readAppId(0),0));
+      logger.info("Status Code [REMOVE SUSCRIPTION APP 3 OF API "+(1)+"]: "+removeSubscriptionRes.statusCode());
+      
+      logger.info("[DEV PORTAL]: Dev Portal tests were completed");
+      
+      
+  }
   
 
   
